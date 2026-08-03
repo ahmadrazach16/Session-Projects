3a5f576ede20d8eb9861390c79b1ee5ab8272761
ahmadraza16 <ahmadraza16ch@gmail.com>
2026-08-03 12:37:00 +0500

    Removed library-management-system

--- Architecture Project/library-management-system/src/shared/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export const logger = (message: string): void => {
-    console.log(`[LOG] ${new Date().toISOString()}: ${message}`);
-};
-
-export const errorHandler = (error: Error): void => {
-    console.error(`[ERROR] ${new Date().toISOString()}: ${error.message}`);
-};
